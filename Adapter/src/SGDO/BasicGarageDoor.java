d66b8c048720b543df4d437b3b2680036e889770
Alexander Hurst <alexander@ahmail.ca>
2018-11-22 12:36:03 -0500

    pushing changes

--- Adapter/src/SGDO/BasicGarageDoor.java
+++ Adapter/src/SGDO/BasicGarageDoor.java
@@ -3,18 +3,17 @@ package SGDO;
 import java.util.logging.Logger;
 
 public class BasicGarageDoor implements StdGarageDoorOpener {
-    private Logger log = Logger.getLogger(
-    		BasicGarageDoor.class.getSimpleName());
-    
+	private Logger log = Logger.getLogger(BasicGarageDoor.class.getSimpleName());
+
 	@Override
 	public void openDoor() {
-    	log.info("Garage Door is Opening");
+		log.info("Garage Door is Opening");
 
 	}
 
 	@Override
 	public void closeDoor() {
-    	log.info("Garage Door is Closing");
+		log.info("Garage Door is Closing");
 
 	}
 
--- Adapter/src/SGDO/PremiumGarageDoorOpener.java
+++ Adapter/src/SGDO/PremiumGarageDoorOpener.java
@@ -5,20 +5,25 @@ import java.util.Map;
 
 public class PremiumGarageDoorOpener {
 
-	private enum FeatureSelection {GARAGEDOOR, LIGHTSWITCH, AIRCOMPRESSOR, CAMERA};
+	private enum FeatureSelection {
+		GARAGEDOOR, LIGHTSWITCH, AIRCOMPRESSOR, CAMERA
+	};
+
 	private Map<FeatureSelection, Object> featureMap;
-	
+
 	public PremiumGarageDoorOpener() {
 		this.featureMap = new HashMap<>();
-        this.featureMap.put(
-        		FeatureSelection.GARAGEDOOR, new GarageDoorDriver());
+		this.featureMap.put(FeatureSelection.GARAGEDOOR, new GarageDoorDriver());
 	}
-	
+
 	public void openDoor(int speedLevel) {
-		((GarageDoorDriver) featureMap.get(FeatureSelection.GARAGEDOOR)).openDoor(speedLevel);;
+		((GarageDoorDriver) featureMap.get(FeatureSelection.GARAGEDOOR)).openDoor(speedLevel);
+		;
 	}
+
 	public void closeDoor(int speedLevel) {
-		((GarageDoorDriver) featureMap.get(FeatureSelection.GARAGEDOOR)).closeDoor(speedLevel);;
+		((GarageDoorDriver) featureMap.get(FeatureSelection.GARAGEDOOR)).closeDoor(speedLevel);
+		;
 
 	}
 }
